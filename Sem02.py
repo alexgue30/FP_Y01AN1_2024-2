9d215b6a97227d5c24492bde88b66555e0a27047
Alex Guevara <100447520+alexgue30@users.noreply.github.com>
2024-09-16 22:09:13 -0500

    Termino de codigo Sem02

---
+++ Sem02.py
@@ -0,0 +1,34 @@
+import streamlit as st
+
+#Inicalizacion de variables
+
+usuarios = []
+
+#Funcion para agregar un usuario
+def agregar_usuario(nombre):
+    usuarios.append(nombre)
+    st.success(f"Usuario {nombre} agregado.")
+
+#Funcion para mostrar usuarios
+def mostrar_usuarios():
+    if usuarios:
+        st.write("Lista de usuarios:")
+        for usuario in usuario:
+            st.white(f"- {usuario}")
+    else:
+        st.warning("No hay usuarios registrados.")
+
+#Menu Principal
+st.title("Gestion de Usuarios")
+
+opcion = st.selectbox("Selecciona una opcion", ["Agregar Usuario", "Mostrar Usuario"])
+
+if opcion == "Agregar Usuario":
+    nombre = st.text_input("Nombre del Usuario")
+    if st.button("Agregar"):
+        if nombre:
+            agregar_usuario(nombre)
+        else
+            st.error("El nombre no puede estar vacio")
+elif opcion == "Mostrar Usuarios":
+    mostar_usuarios()
